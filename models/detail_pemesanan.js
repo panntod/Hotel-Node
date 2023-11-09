e5fed6a54c40c14804a202ddef4733726c012d8e
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-09 10:03:11 +0700

    [fix] rewrite code

--- models/detail_pemesanan.js
+++ models/detail_pemesanan.js
@@ -4,13 +4,9 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class detail_pemesanan extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.pemesanan)
+      this.belongsTo(models.kamar)
     }
   }
   detail_pemesanan.init({
