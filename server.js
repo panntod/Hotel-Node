58fbdee4a90e9ba4f39765fd9cc360cf46bfaa6b
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-14 05:18:38 +0700

    [chore] added endpoint and env config

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const mysql = require("mysql2");
+const dotenv = require('dotenv')
+dotenv.config()
 
 const app = express();
-const port = 3000;
+const port = process.env.APP_PORT;
 app.use(bodyParser.json());
+
+const userRouter = require('./routes/user_route')
+
+app.use(`/user`, userRouter);
 
 app.listen(port, () => {
   console.log(`Server listen to ${port}`);
