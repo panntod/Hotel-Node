9a2db78864e7e691dce5149def4306dddbcbc1a2
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-17 05:24:43 +0700

    [feat] make a function

--- controller/upload_foto.js
+++ controller/upload_foto.js
@@ -8,39 +8,48 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const dir = "./images/user";
-    if (!fs.existsSync(dir)) {
-      fs.mkdirSync(dir);
-    }
-    cb(null, path.join(__dirname, "../images/user"));
-  },
+const createStorage = (destination) =>
+  multer.diskStorage({
+    destination: (req, file, cb) => {
+      const dir = `./images/${destination}`;
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+      cb(null, path.join(__dirname, `./images/${destination}`));
+    },
 
-  filename: (req, file, cb) => {
-    cb(null, `foto-member-${Date.now()}${path.extname(file.originalname)}`);
-  },
-});
+    filename: (req, file, cb) => {
+      cb(
+        null,
+        `${destination}-${Date.now()}${path.extname(file.originalname)}`
+      );
+    },
+  });
 
-const upload = multer({
-  storage: storage,
-  fileFilter: (req, file, cb) => {
-    const acceptedTypes = ["image/jpg", "image/jpeg", "image/png"];
-    if (!acceptedTypes.includes(file.mimetype)) {
-      cb(null, false);
-      return cb(`Invalid file type ${file.mimetype}`);
-    }
+const createMulter = (destination) =>
+  multer({
+    storage: createStorage(destination),
+    fileFilter: (req, file, cb) => {
+      const acceptedTypes = ["image/jpg", "image/jpeg", "image/png"];
+      if (!acceptedTypes.includes(file.mimetype)) {
+        cb(null, false);
+        return cb(`Invalid file type ${file.mimetype}`);
+      }
 
-    const filesize = req.headers["content-length"];
-    const maxSize = 1 * 1024 * 1024;
+      const filesize = req.headers["content-length"];
+      const maxSize = 1 * 1024 * 1024;
 
-    if (filesize > maxSize) {
-      cb(null, false);
-      return cb("File size is too large");
-    }
+      if (filesize > maxSize) {
+        cb(null, false);
+        return cb("File size is too large");
+      }
 
-    cb(null, true);
-  },
-});
+      cb(null, true);
+    },
+  });
 
-module.exports = upload;
+  const uploadUser = createMulter('foto-user')
+
+module.exports = { 
+  uploadUser
+};
