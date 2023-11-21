5e68afe7e713b46f36f932e212f029e9ad19f1f1
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-21 09:10:26 +0700

    [doc] added kamar endpoint

--- server.js
+++ server.js
@@ -9,8 +9,10 @@ const port = process.env.APP_PORT;
 app.use(bodyParser.json());
 
 const userRouter = require('./routes/user_route')
+const kamarRouter = require('./routes/kamar_route')
 
 app.use(`/user`, userRouter);
+app.use(`/kamar`, kamarRouter);
 
 app.listen(port, () => {
   console.log(`Server listen to ${port}`);
