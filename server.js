752d339aa90d54fa150831af1f5a487e232dcfab
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-04 12:56:56 +0700

    [chore] added cors, static express, login route

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const mysql = require("mysql2");
-const dotenv = require('dotenv')
-dotenv.config()
+const dotenv = require("dotenv").config();
+const cors = require(`cors`);
 
 const app = express();
 const port = process.env.APP_PORT;
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:3003"],
+    credentials: true,
+  })
+);
 
-const userRouter = require('./routes/user_route')
-const kamarRouter = require('./routes/kamar_route')
-const tipeKamarRouter = require('./routes/tipe_kamar_route')
+const userRouter = require("./routes/user_route");
+const kamarRouter = require("./routes/kamar_route");
+const tipeKamarRouter = require("./routes/tipe_kamar_route");
+const loginRouter = require("./routes/login_route")
 
 app.use(`/user`, userRouter);
 app.use(`/kamar`, kamarRouter);
 app.use(`/tipe`, tipeKamarRouter);
+app.use(`/login`, loginRouter)
+
+app.use(express.static(__dirname));
+app.use(express.static("images/foto-tipe-kamar"));
+app.use(express.static("images/foto-user"));
 
 app.listen(port, () => {
   console.log(`Server listen to ${port}`);
