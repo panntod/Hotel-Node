eb48f9213e25ab66dfe82904fb97147b558467f2
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-04 12:58:38 +0700

    [feat] added middleware

--- routes/tipe_kamar_route.js
+++ routes/tipe_kamar_route.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 
 const tipeController = require("../controller/tipe_kamar_controller");
+const auth = require("../Middleware/Auth");
 
-app.get("/", tipeController.getAllType);
-app.get("/find/:id", tipeController.findType);
-app.post("/", tipeController.addType);
-app.delete("/:id", tipeController.deleteType);
-app.put("/:id", tipeController.updateTypeKamar);
+app.get("/", auth.authVerify, tipeController.getAllType);
+app.get("/find/:id", auth.authVerify, tipeController.findType);
+app.post("/", auth.authVerify, tipeController.addType);
+app.delete("/:id", auth.authVerify, tipeController.deleteType);
+app.put("/:id", auth.authVerify, tipeController.updateTypeKamar);
 
 module.exports = app;
