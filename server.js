6a4b3bdc54904d74da0056d7e3cd9137f7988dc3
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-12-13 18:39:59 +0700

    feat: change credentials uri

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3003"],
+    origin: ["http://localhost:3000", "http://localhost:5173"],
     credentials: true,
   })
 );
